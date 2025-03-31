b1619905ca091bf7223faba2c2e8836367f44c16
Rafael Ramalho Silveira <rafael@techrafael.com.br>
2025-03-31 07:43:05 -0400

    Projeto Inicial

---
+++ src/main/java/br/gov/mt/seplag/cadastro_servidores/CadastroServidoresApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.cadastro_servidores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroServidoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroServidoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/mt/seplag/cadastro_servidores/CadastroServidoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.cadastro_servidores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroServidoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
